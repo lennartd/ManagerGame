c9ffe9e786a0c77abe24f13f311a208abdd7944f
Lennart Dümmel <lennart_duemmel@gmx.de>
2013-10-02 14:51:49 +0200

    added EmailToMessageConverter

---
+++ ManagerGame/EmailToMessageConverter.vb
@@ -0,0 +1,12 @@
+﻿Public Class EmailToMessageConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+        Dim emailsCount As Integer = DirectCast(value, Integer)
+        Return emailsCount & " neue E-Mails"
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Return Nothing
+    End Function
+End Class
